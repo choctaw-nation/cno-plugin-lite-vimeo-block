a2a4544c6ac4cf0fb8fe434fd33cccbaf9a6bedd
KJ Roelke <kroelke@choctawnation.com>
2025-05-20 16:21:30 -0500

    remove build folder

--- build/cno-plugin-lite-vimeo-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '6e3b44e014cfd107c9a7');
--- build/cno-plugin-lite-vimeo-block/script.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '7f45b1ca6e96ccacd2d1');
